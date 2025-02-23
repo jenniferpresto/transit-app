8aacec690f1563c41c4da6534c9e76e35329d59d
Jennifer Presto <sandlappernyc@yahoo.com>
2025-02-23 17:51:50 -0500

    added linear fade

--- neoPixels.py
+++ neoPixels.py
@@ -5,11 +5,23 @@ import neopixel
 pixels = neopixel.NeoPixel(board.D18, 1, pixel_order=neopixel.RGB)
 pixels[0] = (0, 0, 255)
 
-while True:
-    pixels.fill((255, 0, 0))
-    time.sleep(1)
-    pixels.fill((0, 255, 0))
-    time.sleep(1)
-    pixels.fill((0, 0, 255))
-    time.sleep(1)
+framerate = 30
+brightness = 1
+
+try:
+    while True:
+        for i in range(framerate):
+            brightness = 1 - i / framerate
+            print (brightness)
+            pixels.fill((255 * brightness, 0, 0))
+            time.sleep(1 / framerate)
+        for i in range(framerate):
+            brightness = 1 - i / framerate
+            pixels.fill((255 * brightness, 255 * brightness, 0))
+            time.sleep(1 / framerate)
+        for i in range(framerate):
+            brightness = 1 - i / framerate
+            pixels.fill((0, 0, 255 * brightness))
+            time.sleep(1 / framerate)
+except KeyboardInterrupt:
     pixels.fill((0, 0, 0))
